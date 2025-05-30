d68015d13723bc019f48e19d52954fb5f367f4ae
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2025-05-30 12:51:48 -0300

    line limiting

--- Files/Trabalhos/PA2/source/indexer.py
+++ Files/Trabalhos/PA2/source/indexer.py
@@ -3,7 +3,8 @@
 
 ## Indexer
 
-Your implementation must include an `indexer.py` file, which will be executed in the same virtual environment described above, as follows:
+Your implementation must include an `indexer.py` file, which will be executed in the same virtual
+environment described above, as follows:
 
 ```bash
 python3 indexer.py -m <MEMORY> -c <CORPUS> -i <INDEX>
@@ -15,7 +16,8 @@ with the following arguments:
 - `-c <CORPUS>`: the path to the corpus file to be indexed.
 - `-i <INDEX>`: the path to the directory where indexes should be written.
 
-At the end of the execution, your indexer.py implementation must print a JSON document to standard output [$^1$][Note_1] with the following statistics:
+At the end of the execution, your indexer.py implementation must print a JSON document to standard
+output [$^1$][Note_1] with the following statistics:
 
 - `Index Size`: the index size in megabytes;
 - `Elapsed Time`: the time elapsed (in seconds) to produce the index;
@@ -35,26 +37,53 @@ The following example illustrates the required output format:
 
 ### Document Corpus
 
-The corpus to be indexed comprises structured representations (with id, title, descriptive text, and keywords) for a total of 4,641,784 named entities present in Wikipedia. These structured representations are encoded as `JSON` documents in a single `JSONL` file available for download.[$^2$][Note_2] To speed up development, you are encouraged to use a smaller portion of the corpus to test your implementation before you try to index the complete version.
+The corpus to be indexed comprises structured representations (with id, title, descriptive text,
+and keywords) for a total of 4,641,784 named entities present in Wikipedia. These structured
+representations are encoded as `JSON` documents in a single `JSONL` file available for
+download.[$^2$][Note_2] To speed up development, you are encouraged to use a smaller portion of the
+corpus to test your implementation before you try to index the complete version.
 
 ### Indexing Policies
 
-For each document in the corpus (the `-c` argument above), your implementation must parse, tokenize, and index it. Your implementation must operate within the designated memory budget (the `-m` argument) during its entire execution. [$^3$][Note_3] [$^4$][Note_4] This emulates the most typical scenario where the target corpus far exceeds the amount of physical memory available to the indexer. At the end of the execution, a final representation of all produced index structures (inverted index, document index, term lexicon) must be stored as three separate files, one for each structure, at the designated directory (the `-i` argument).
+For each document in the corpus (the `-c` argument above), your implementation must parse, tokenize,
+and index it. Your implementation must operate within the designated memory budget (the `-m`
+argument) during its entire execution. [$^3$][Note_3] [$^4$][Note_4] This emulates the most typical
+scenario where the target corpus far exceeds the amount of physical memory available to the indexer.
+At the end of the execution, a final representation of all produced index structures (inverted
+index, document index, term lexicon) must be stored as three separate files, one for each structure,
+at the designated directory (the `-i` argument).
 
-In addition to this workflow, your implementation **must abide by the following policies**, which will determine your final grade in this assignment:
+In addition to this workflow, your implementation **must abide by the following policies**, which
+will determine your final grade in this assignment:
 
-1. _Pre-processing Policy_: To reduce the index size, your implementation **must perform stopword removal and stemming**. Additional preprocessing techniques can be implemented at your discretion.
+1. _Pre-processing Policy_: To reduce the index size, your implementation **must perform stopword
+removal and stemming**. Additional preprocessing techniques can be implemented at your discretion.
 
-2. _Memory Management Policy_: To ensure robustness, your implementation **must execute under limited memory availability**. To this end, it must be able to produce partial indexes in memory (respecting the imposed memory budget) and merge them on disk. [$^5$][Note_5]
+2. _Memory Management Policy_: To ensure robustness, your implementation **must execute under
+limited memory availability**. To this end, it must be able to produce partial indexes in memory
+(respecting the imposed memory budget) and merge them on disk. [$^5$][Note_5]
 
-3. _Parallelization Policy_: To ensure maximum efficiency, you **must parallelize the indexing process across multiple threads**. You may experiment to find an optimal number of threads to minimize indexing time rate while minimizing the incurred parallelization overhead.
+3. _Parallelization Policy_: To ensure maximum efficiency, you **must parallelize the indexing
+process across multiple threads**. You may experiment to find an optimal number of threads to
+minimize indexing time rate while minimizing the incurred parallelization overhead.
 
-4. _Compression Policy (extra)_: Optionally, **you may choose to implement a compression scheme for index entries** (e.g. gamma for docids, unary for term frequency) for maximum storage efficiency.
+4. _Compression Policy (extra)_: Optionally, **you may choose to implement a compression scheme
+for index entries** (e.g. gamma for docids, unary for term frequency) for maximum storage
+efficiency.
 
 [Note_1]: https://en.wikipedia.org/wiki/Standard_streams#Standard_output_(stdout)
+
 [Note_2]: https://www.kaggle.com/datasets/rodrygo/entities
-[Note_3]: <> "The memory limit will be strictly enforced during grading. If your program exceeds it, it may be automatically terminated with an out-of-memory (OOM) error. To prevent this, use `psutil.Process(os.getpid()).memory_info().rss` to monitor your current memory usage (in bytes), and offload partial indexes to disk before allocating more memory as needed."
-[Note_4]: <> "Note that the memory budget refers to the total memory available to your implementation, not only to the memory needed to store the actual index structures. As a reference lower bound, assume your implementation will be tested with `-m 1024`."
+
+[Note_3]: <> "The memory limit will be strictly enforced during grading. If your program exceeds it,
+it may be automatically terminated with an out-of-memory (OOM) error. To prevent this, use
+`psutil.Process(os.getpid()).memory_info().rss` to monitor your current memory usage (in bytes), and
+offload partial indexes to disk before allocating more memory as needed."
+
+[Note_4]: <> "Note that the memory budget refers to the total memory available to your
+implementation, not only to the memory needed to store the actual index structures. As a reference
+lower bound, assume your implementation will be tested with `-m 1024`."
+
 [Note_5]: https://en.wikipedia.org/wiki/External_sorting#External_merge_sort
 
 """
