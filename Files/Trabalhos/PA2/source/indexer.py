8ae36759bf4ff8d7dc4a3ca8464565b2e6c257d3
João Vítor Fernandes Dias <joaovitorfd2000@gmail.com>
2025-05-31 17:49:59 -0300

    pre indexer

--- Files/Trabalhos/PA2/source/indexer.py
+++ Files/Trabalhos/PA2/source/indexer.py
@@ -71,7 +71,8 @@ minimize indexing time rate while minimizing the incurred parallelization overhe
 for index entries** (e.g. gamma for docids, unary for term frequency) for maximum storage
 efficiency.
 
-[Note_1]: https://en.wikipedia.org/wiki/Standard_streams#Standard_output_(stdout)
+#Standard_output_(stdout)
+[Note_1]: https://en.wikipedia.org/wiki/Standard_streams
 
 [Note_2]: https://www.kaggle.com/datasets/rodrygo/entities
 
@@ -92,9 +93,13 @@ import time  # Time tracking
 import json  # Pretty prints JSON
 import argparse  # Argument parsing
 import os  # File operations
+import nltk  # Natural Language Toolkit for text processing
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import psutil  # Process and system utilities for memory management
 
-START_TIME = time.time()
-INDEX_PATH = "index.json"  # Default index path
+FORCE_CREATE = True  # Force creation of index files if they already exist
 
 
 def get_indexer_args():
@@ -105,14 +110,15 @@ def get_indexer_args():
     """
 
     parser = argparse.ArgumentParser(description="Indexer arguments")
-    parser.add_argument("-m", "--memory", type=int,
-                        required=True, help="Memory in MB")
-    parser.add_argument("-c", "--corpus", type=str,
-                        required=True, help="Corpus path")
-    parser.add_argument("-i", "--index", type=str,
-                        required=True, help="Index path")
+    parser.add_argument("-m", "--memory", help="Memory in MB",
+                        required=True, type=int)
+    parser.add_argument("-c", "--corpus", help="Corpus path",
+                        required=True, type=str)
+    parser.add_argument("-i", "--index", help="Index path",
+                        required=True, type=str)
     args = parser.parse_args()
     args_dict = vars(args)
+
     return args_dict
 
 
@@ -180,11 +186,183 @@ def print_json(data):
     print(json.dumps(data, indent=4))
 
 
+def generate_structures(index_path):
+    """ Generate the index structures (inverted index, document index, term lexicon) """
+
+    def create_file(file_path):
+        """ Create a file if it does not exist """
+        if not os.path.exists(file_path) or FORCE_CREATE:
+            with open(file_path, 'w', encoding='utf8') as f:
+                json.dump({}, f, indent=4)
+
+    def generate_inverted_index(output_dir):
+        """ Generate the inverted index """
+        # Placeholder for actual implementation
+        inverted_index_path = os.path.join(output_dir, 'inverted_index.json')
+        create_file(inverted_index_path)
+
+    def generate_document_index(output_dir):
+        """ Generate the document index """
+        document_index_path = os.path.join(output_dir, 'document_index.json')
+        create_file(document_index_path)
+
+    def generate_term_lexicon(output_dir):
+        """ Generate the term lexicon """
+        term_lexicon_path = os.path.join(output_dir, 'term_lexicon.json')
+        create_file(term_lexicon_path)
+
+    generate_inverted_index(index_path)
+    generate_document_index(index_path)
+    generate_term_lexicon(index_path)
+
+
+def pre_processing(doc):
+    """ Pre-process the document (stopword removal and stemming) """
+
+    def tokenize(doc):
+        """ Use NLTK to tokenize the document """
+        # Ensure NLTK resources are available
+        nltk.download('punkt', quiet=True)
+
+        # Tokenize the document text
+        doc['text'] = word_tokenize(doc['text'])
+        doc['title'] = word_tokenize(doc['title'])
+        doc['keywords'] = [word_tokenize(keyword)
+                           for keyword in doc['keywords']]
+
+    def remove_stopwords(doc):
+        """ Remove stopwords from the document """
+
+        def get_stopwords():
+            """ Get previously defined stopwords """
+            nltk.download('stopwords', quiet=True)
+            stop_words = set(stopwords.words('english'))
+            # with open('source/input/stopwords.json', 'r', encoding='utf8') as f:
+            #     stop_words = json.load(f)
+            return stop_words
+
+        def remove_stopwords_from_list(word_list, stop_words):
+            """ Remove stopwords from a list of words """
+            return [word for word in word_list if word.lower() not in stop_words]
+
+        stop_words = get_stopwords()
+
+        doc['text'] = remove_stopwords_from_list(doc['text'], stop_words)
+        doc['title'] = remove_stopwords_from_list(doc['title'], stop_words)
+        doc['keywords'] = [remove_stopwords_from_list(keywords, stop_words)
+                           for keywords in doc['keywords']]
+
+        # print(my_stopwords)
+
+    def stem(doc):
+        """ Stem the document using NLTK's english stemmer """
+
+        # Ensure NLTK resources are available
+        nltk.download('punkt', quiet=True)
+
+        stemmer = PorterStemmer()
+
+        def stem_list(word_list):
+            """ Stem a list of words """
+            return [stemmer.stem(word) for word in word_list]
+
+        doc['text'] = stem_list(doc['text'])
+        doc['title'] = stem_list(doc['title'])
+        doc['keywords'] = [stem_list(keywords) for keywords in doc['keywords']]
+
+    # doc = tokenize(doc)
+    tokenize(doc)
+    # print("Tokenized document:")
+    # print(len(doc['text']), len(doc['title']), list(map(len, doc['keywords'])))
+
+    remove_stopwords(doc)
+    # print("Removing stopwords from document:")
+    # print(len(doc['text']), len(doc['title']), list(map(len, doc['keywords'])))
+
+    stem(doc)
+
+
+def append_to_structures(doc, index_path):
+    """ Append the processed document to the index structures """
+
+    def append_to_inverted_index(doc, output_dir):
+        """ Add inverted index entries for the document
+            Only the text field is indexed, title and keywords are not indexed
+            Also, only the existance of the term is indexed, not its frequency
+        """
+        inverted_index_path = os.path.join(output_dir, 'inverted_index.json')
+
+        # Load existing inverted index or create a new one
+        if os.path.exists(inverted_index_path):
+            with open(inverted_index_path, 'r', encoding='utf8') as f:
+                inverted_index = json.load(f)
+        else:
+            inverted_index = {}
+
+        # Add entries for each term in the document
+        id = int(doc['id'])
+        for term in doc['text']:
+            if term not in inverted_index:
+                inverted_index[term] = []
+            inverted_index[term].append(id)
+
+        # # Save the updated inverted index
+        with open(inverted_index_path, 'w', encoding='utf8') as f:
+            json.dump(inverted_index, f, indent=4)
+
+    def append_to_document_index(doc_idx, output_dir):
+        """ Add document index entries for the document """
+
+    def append_to_term_lexicon(doc_term_lex, output_dir):
+        """ Add term lexicon entries for the document """
+
+    output_dir = os.path.dirname(index_path)
+
+    append_to_inverted_index(doc, output_dir)
+    # append_to_document_index(doc, output_dir)
+    # append_to_term_lexicon(doc, output_dir)
+
+
+def doc_processing(doc, mem_limit, index_path):
+    """ Process a single document """
+
+    doc = json.loads(doc)
+
+    pre_processing(doc)  # tokenizing, stopword removal and stemming
+
+    append_to_structures(doc, index_path)
+
+
+def indexer(cmd_args):
+    """ Main indexer function """
+
+    generate_structures(cmd_args['index'])
+
+    limit = 2
+    idx = 0
+
+    with open(cmd_args['corpus'], 'r', encoding='utf8') as corpus_file:
+        for doc in corpus_file:
+            if idx >= limit:
+                break
+            idx += 1
+            # doc_processing(doc, cmd_args['memory'], cmd_args['index'])
+
+
 def main():
-    """ indexer base """
-    get_indexer_args()
-    statistics = compute_statistics(START_TIME, INDEX_PATH)
-    print_json(statistics)
+    """
+        - Gets the arguments
+        - starts perfomance tracking
+        - calls the indexer function
+        - later prints the final statistics
+    """
+    cmd_args = get_indexer_args()
+    start_time = time.time()  # Start time for statistics
+
+    indexer(cmd_args)
+
+    # statistics = compute_statistics(start_time, cmd_args['index'])
+    # print_json(statistics)
 
 
 main()
